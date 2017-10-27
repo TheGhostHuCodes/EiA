59d46f6ada3a966347d1c87e47f79be61a2a9596
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-10-27 10:39:40 -0700

    Refactor out image links into a model

--- PhotoGroove/PhotoGroove.elm
+++ PhotoGroove/PhotoGroove.elm
@@ -4,16 +4,27 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "https://imgs.xkcd.com/comics/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src " https://imgs.xkcd.com/comics/cast_iron_pans.png " ] []
-            , img [ src "https://imgs.xkcd.com/comics/making_progress.png" ] []
-            , img [ src "https://imgs.xkcd.com/comics/immune_system.png" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail model)
         ]
 
 
+viewThumbnail thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url) ] []
+
+
+initialModel =
+    [ { url = "cast_iron_pans.png" }
+    , { url = "making_progress.png" }
+    , { url = "immune_system.png" }
+    ]
+
+
 main =
-    view "no model yet"
+    view initialModel
