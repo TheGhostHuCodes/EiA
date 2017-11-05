7442288976a409f2eb870d82621712e90262bb2b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-11-04 17:48:13 -0700

    Initialize status message by display Pasta version number

--- PhotoGroove/PhotoGroove.elm
+++ PhotoGroove/PhotoGroove.elm
@@ -250,14 +250,23 @@ viewOrError model =
                 ]
 
 
-main : Program Never Model Msg
+main : Program Float Model Msg
 main =
-    Html.program
-        { init = ( initialModel, initialCmd )
+    Html.programWithFlags
+        { init = init
         , view = viewOrError
         , update = update
         , subscriptions = (\_ -> statusChanges SetStatus)
         }
+
+
+init : Float -> ( Model, Cmd Msg )
+init flags =
+    let
+        status =
+            "initializing Pasta v" ++ toString flags
+    in
+        ( { initialModel | status = status }, initialCmd )
 
 
 paperSlider : List (Attribute msg) -> List (Html msg) -> Html msg
