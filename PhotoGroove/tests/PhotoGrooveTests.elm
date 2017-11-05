e10dcb70eeac6ccd2439c49a39b61c3dea4b0643
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-11-05 08:19:02 -0800

    Add fuzz test for update through SelectByUrl

--- PhotoGroove/tests/PhotoGrooveTests.elm
+++ PhotoGroove/tests/PhotoGrooveTests.elm
@@ -19,3 +19,14 @@ decoderTest =
                 |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+selectByUrlSelectsPhoto : Test
+selectByUrlSelectsPhoto =
+    fuzz string "SelectByUrl selects the given photo by URL" <|
+        \url ->
+            PhotoGroove.initialModel
+                |> PhotoGroove.update (SelectByUrl url)
+                |> Tuple.first
+                |> .selectedUrl
+                |> Expect.equal (Just url)
